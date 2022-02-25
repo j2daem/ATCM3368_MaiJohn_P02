e0be041e8e8c379c34cfb4e6cac07351af5b4b94
j2daem <62685566+j2daem@users.noreply.github.com>
2022-02-25 13:56:19 -0600

    Reworked menu to use enum states
    Reworked menu to change based on enumerated menu states. Functioning credits and quit screen, but reset needs to be redone after figuring out the reset function. Start also needs to be reimplemented.
    
    Needs a change of aesthetics later too.

---
+++ ATCM3368_MaiJohn_P02/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] MenuManager menuManager = null;
+
+    private MenuState currentMenuState;
+
+    private void Start()
+    {
+        currentMenuState = MenuState.Main;
+    }
+
+    public enum MenuState
+    {
+        Main,
+        Credits,
+        Reset,
+        Quit
+    }
+
+    public void MenuToOpen(string _menuName)
+    {
+        switch (_menuName)
+        {
+            case "Main":
+            case "main":
+                currentMenuState = MenuState.Main;
+                Debug.Log("Current menu state: Main");
+                break;
+
+            case "Credits":
+            case "credits":
+                currentMenuState = MenuState.Credits;
+                Debug.Log("Current menu state: Credits");
+                break;
+
+            case "Reset":
+            case "reset":
+                currentMenuState = MenuState.Reset;
+                Debug.Log("Current menu state: Reset");
+                break;
+
+            case "Quit":
+            case "quit":
+                currentMenuState = MenuState.Quit;
+                Debug.Log("Current menu state: Quit");
+                break;
+
+            default:
+                Debug.Log("Invalid menu state.");
+                break;
+        }
+        menuManager.ChangeMenuTo(currentMenuState);
+    }
+
+    public void ConfirmQuit()
+    {
+        Application.Quit();
+    }
+}
---
+++ ATCM3368_MaiJohn_P02/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenuPanel = null;
+    [SerializeField] GameObject creditsMenuPanel = null;
+    [SerializeField] GameObject resetMenuPanel = null;
+    [SerializeField] GameObject quitMenuPanel = null;
+
+    GameObject currentMenu = null;
+
+    public void ChangeMenuTo(GameManager.MenuState _menuName)
+    {
+        switch (_menuName)
+        {
+            case GameManager.MenuState.Main:
+                mainMenuPanel.SetActive(true);
+                creditsMenuPanel.SetActive(false);
+                resetMenuPanel.SetActive(false);
+                quitMenuPanel.SetActive(false);
+                break;
+
+            case GameManager.MenuState.Credits:
+                creditsMenuPanel.SetActive(true);
+                mainMenuPanel.SetActive(false);
+                resetMenuPanel.SetActive(false);
+                quitMenuPanel.SetActive(false);
+                break;
+
+            case GameManager.MenuState.Reset:
+                resetMenuPanel.SetActive(true);
+                mainMenuPanel.SetActive(false);
+                creditsMenuPanel.SetActive(false);
+                quitMenuPanel.SetActive(false);
+                break;
+
+            case GameManager.MenuState.Quit:
+                quitMenuPanel.SetActive(true);
+                mainMenuPanel.SetActive(false);
+                creditsMenuPanel.SetActive(false);
+                resetMenuPanel.SetActive(false);
+                break;
+        }
+    }
+}
