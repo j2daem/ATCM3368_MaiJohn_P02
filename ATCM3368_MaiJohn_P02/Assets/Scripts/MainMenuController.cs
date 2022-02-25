4f45601bbdb007f147f37869b3cc88ef650f7882
j2daem <62685566+j2daem@users.noreply.github.com>
2022-02-25 16:14:38 -0600

    Implemented fully functional menu screen
    Finished the menu screen. Moved game manager code to MainMenu manager because there was not point in separating. Moved the methods for button functions to the MainMenuController to stay organized and consistent. Controller are for actually performing duties whereas managers should be in charge or allocating resources.

--- ATCM3368_MaiJohn_P02/Assets/Scripts/MainMenuController.cs
+++ ATCM3368_MaiJohn_P02/Assets/Scripts/MainMenuController.cs
@@ -19,4 +19,18 @@ public class MainMenuController : MonoBehaviour
             AudioManager.Instance.PlaySong(_startingSong);
         }
     }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        Debug.Log("High score reset.");
+
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- ATCM3368_MaiJohn_P02/Assets/Scripts/MenuManager.cs
+++ ATCM3368_MaiJohn_P02/Assets/Scripts/MenuManager.cs
@@ -4,39 +4,91 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    //Get menu panels to enable/disable
     [SerializeField] GameObject mainMenuPanel = null;
     [SerializeField] GameObject creditsMenuPanel = null;
     [SerializeField] GameObject resetMenuPanel = null;
     [SerializeField] GameObject quitMenuPanel = null;
 
-    GameObject currentMenu = null;
+    //Keep track which state is active
+    private MenuState currentMenuState;
 
-    public void ChangeMenuTo(GameManager.MenuState _menuName)
+    private void Start()
+    {
+        //Start with the Main Menu panel active
+        currentMenuState = MenuState.Main;
+    }
+
+    //Menu choices (choices limited through enums)
+    public enum MenuState
+    {
+        Main,
+        Credits,
+        Reset,
+        Quit
+    }
+
+    public void ChooseMenuToOpen(string _menuName)
     {
         switch (_menuName)
         {
-            case GameManager.MenuState.Main:
+            case "Main":
+            case "main":
+                currentMenuState = MenuState.Main;
+                Debug.Log("Current menu state: Main");
+                break;
+
+            case "Credits":
+            case "credits":
+                currentMenuState = MenuState.Credits;
+                Debug.Log("Current menu state: Credits");
+                break;
+
+            case "Reset":
+            case "reset":
+                currentMenuState = MenuState.Reset;
+                Debug.Log("Current menu state: Reset");
+                break;
+
+            case "Quit":
+            case "quit":
+                currentMenuState = MenuState.Quit;
+                Debug.Log("Current menu state: Quit");
+                break;
+
+            default:
+                Debug.Log("Invalid menu state.");
+                break;
+        }
+        ChangeMenuTo(currentMenuState);
+    }
+
+    public void ChangeMenuTo(MenuState _menuName)
+    {
+        switch (_menuName)
+        {
+            case MenuState.Main:
                 mainMenuPanel.SetActive(true);
                 creditsMenuPanel.SetActive(false);
                 resetMenuPanel.SetActive(false);
                 quitMenuPanel.SetActive(false);
                 break;
 
-            case GameManager.MenuState.Credits:
+            case MenuState.Credits:
                 creditsMenuPanel.SetActive(true);
                 mainMenuPanel.SetActive(false);
                 resetMenuPanel.SetActive(false);
                 quitMenuPanel.SetActive(false);
                 break;
 
-            case GameManager.MenuState.Reset:
+            case MenuState.Reset:
                 resetMenuPanel.SetActive(true);
                 mainMenuPanel.SetActive(false);
                 creditsMenuPanel.SetActive(false);
                 quitMenuPanel.SetActive(false);
                 break;
 
-            case GameManager.MenuState.Quit:
+            case MenuState.Quit:
                 quitMenuPanel.SetActive(true);
                 mainMenuPanel.SetActive(false);
                 creditsMenuPanel.SetActive(false);
